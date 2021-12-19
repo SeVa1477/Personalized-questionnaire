3b94afe64c307865e68f949ae231ff2d613026c6
SerVas <Smith1477@yandex.ru>
2021-12-19 16:00:39 +0300

    Initial Commit

---
+++ logInProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  logInProfile
+//
+//  Created by Ser Vas on 19.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
