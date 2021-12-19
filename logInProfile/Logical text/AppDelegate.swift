11aa52ffbc9c816b416908c61ac1eae6ae70775b
SerVas <Smith1477@yandex.ru>
2021-12-19 23:50:29 +0300

    It was possible to write a lot about myself, but I could not figure out how to implement it all ((((

---
+++ logInProfile/Logical text/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  logInProfile
+//
+//  Created by Ser Vas on 19.12.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
